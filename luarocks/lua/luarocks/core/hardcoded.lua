e30b9e5eee3c70a6231053a77977c0fac2e67c26
kyrtion <kyrtion@gmail.com>
2022-06-26 16:35:16 +0300

    added libs for lua

---
+++ luarocks/lua/luarocks/core/hardcoded.lua
@@ -0,0 +1,13 @@
+return {
+	SYSCONFDIR = [[C:\Games\mhrp\bin\SAMP\moonloader\luarocks]],
+	SYSTEM = [[windows]],
+	FORCE_CONFIG = true,
+	LUA_INCDIR = [[C:\Games\mhrp\bin\SAMP\moonloader\luajit\inc]],
+	LUA_LIBDIR = [[C:\Games\mhrp\bin\SAMP\moonloader\luajit\lib]],
+	LUA_DIR = [[C:\Games\mhrp\bin\SAMP\moonloader\luajit]],
+	LUA_INTERPRETER = [[luajit.exe]],
+	WIN_TOOLS = [[C:\Games\mhrp\bin\SAMP\moonloader\luarocks\tools]],
+	LUA_BINDIR = [[C:\Games\mhrp\bin\SAMP\moonloader\luajit\bin]],
+	PROCESSOR = [[x86]],
+	PREFIX = [[C:\Games\mhrp\bin\SAMP\moonloader\luarocks]],
+}
