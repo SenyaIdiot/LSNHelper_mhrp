a66bca2f006ff99e7edaf11a0dff88506c4310c2
Mikhail Potemkin <kyrtion@gmail.com>
2022-07-14 20:14:16 +0300

    bumb version

--- LSN-Helper.lua
+++ LSN-Helper.lua
@@ -2,7 +2,7 @@ script_name('LSN-Helper')
 script_description('Los Santos News Helper (LSNH) for special project MyHome RP')
 script_author('kyrtion#7310')
 script_properties('work-in-pause')
-script_version('2.4')
+script_version('3.0')
 
 require 'lib.moonloader'
 local dlstatus = require('moonloader').download_status
